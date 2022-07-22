26d0a326022eb0851407e5dd1b3248f07b3174dc
Dicky Muhamad R <dickymr.dev@gmail.com>
2022-07-22 15:42:19 +0700

    change text

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -44,7 +44,7 @@ const Projects = () => {
       <Text fontSize="2xl" fontWeight={'bold'} mb={2}>
         Projects
       </Text>
-      <Text mb={10}>Here are a few projects I've worked on recently</Text>
+      <Text mb={10}>{`Here are a few projects I've worked on recently`}</Text>
       <SearchBox type={'project'} />
       <Flex justify={'space-between'} flexWrap={'wrap'}>
         {isEmpty ? (
