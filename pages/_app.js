b94db49fab1f282e3dc8376c27e291ca48e1b419
Dicky Muhamad R <dickymr.dev@gmail.com>
2022-07-22 15:36:15 +0700

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../assets/css/index.css';
+import Script from 'next/script';
 import { ChakraProvider } from '@chakra-ui/react';
 import Layout from '../layout';
 import theme from '../theme';
@@ -9,6 +10,24 @@ function MyApp(props) {
 
   return (
     <ChakraProvider theme={theme}>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script
+        id="analytics"
+        strategy="lazyOnload"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <DefaultSeo
         title="Personal Website | Dicky Muhamad R"
         description="Frontend Web Developer"
