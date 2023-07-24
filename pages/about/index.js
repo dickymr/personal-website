4e909bf7fefc5a515375d564655f31e18625e38c
Dicky Muhamad R <rizkyd@garena.co.id>
2023-07-24 04:41:52 +0000

    remove ssr

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,37 +1,39 @@
 import { NextSeo } from 'next-seo';
+import { useQuery } from '@tanstack/react-query';
 import { Avatar, Box, Flex, Link, Text } from '@chakra-ui/react';
+
 import { Breadcrumb, Markdown } from '../../components';
+import { fetcher } from '../../utils';
 
-export const getServerSideProps = async () => {
-  const api = `${process.env.NEXT_PUBLIC_API_URL}${'/api/profile?populate=*'}`;
-  const res = await fetch(api);
-  const profile = await res.json();
+const About = () => {
+  const { data: profile, isLoading } = useQuery({
+    queryKey: ['my_profile'],
+    queryFn: async () => fetcher('/api/profile?populate=*'),
+  });
 
-  return {
-    props: { profile: profile.data },
-  };
-};
+  if (isLoading) {
+    return null;
+  }
 
-const About = ({ profile }) => {
   return (
     <Box>
       <NextSeo title="About | Dicky Muhamad R" />
       <Breadcrumb items={['home', 'about']} />
       <Flex my={10} direction={{ base: 'column', md: 'row' }} align="center">
-        <Avatar size="xl" name={profile?.name} src={profile?.avatar?.url} mr={5} />
+        <Avatar size="xl" name={profile?.data?.name} src={profile?.data?.avatar?.url} mr={5} />
         <Flex direction={'column'} justify="center" align={{ base: 'center', md: 'flex-start' }}>
           <Text fontSize={'2xl'} fontWeight={'bold'} mt={{ base: 3, md: 0 }}>
-            {profile?.name}
+            {profile?.data?.name}
           </Text>
           <Text>
-            {profile?.title} at{' '}
-            <Link href={profile?.company_url} isExternal>
-              {profile?.company}
+            {profile?.data?.title} at{' '}
+            <Link href={profile?.data?.company_url} isExternal>
+              {profile?.data?.company}
             </Link>
           </Text>
         </Flex>
       </Flex>
-      <Markdown content={profile?.long_desc} />
+      <Markdown content={profile?.data?.long_desc} />
     </Box>
   );
 };
